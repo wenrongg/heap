ffb6df9b5107d43566dfb48a62f6ea617744a9b5
wenrongg <67977685+wenrongg@users.noreply.github.com>
2020-12-09 01:11:29 -0500

    Add files via upload

---
+++ heap.py
@@ -0,0 +1,87 @@
+import heapq
+
+def left(index):
+    '''Return index's left child index.
+    '''
+    return index * 2 + 1
+
+
+def right(index):
+    '''Return index's left child index.
+    '''
+    return index * 2 + 2
+
+
+def parent(index):
+    '''Return index's parent index.'''
+
+    return (index - 1) // 2
+
+class EmptyHeapException(Exception):
+    pass
+
+class MinHeap:
+
+    def __init__(self, L=None):
+        '''Create a new MinHeap.
+        This method is complete.'''
+
+        if not L:        
+            self._data = []
+        else:
+            self._data = L
+            self._min_heapify()
+
+
+    def __len__(self):
+        '''Return the length of the MinHeap.
+        This method is complete.'''
+
+        return len(self._data)
+
+
+    def __str__(self):
+        '''Return a string representation of the heap.
+        This method is complete.'''
+
+        return str(self._data)
+
+
+    def insert(self, v):
+        '''Insert v in self. Maintain heap property.'''
+           
+        return heapq.heappush(self._data,v)
+
+
+    def extract_min(self):
+        '''Remove minimal value in self. Restore heap property.
+        Raise EmptyHeapException if heap is empty.'''
+        if len(self._data) == 0:
+            raise EmptyHeapException
+        else: 
+            return heapq.heappop(self._data)
+
+
+    def _percolate_up(self):
+        '''Restore heap property of self after 
+        adding new item'''
+
+        pass
+
+
+    def _percolate_down(self, i):
+        ''' Restore heap property of subtree 
+        rooted at index i.
+        '''
+
+        # while larger than at least one child
+        # swap with smaller child and repeat    
+        pass
+
+
+    def _min_heapify(self):
+        '''Turn unordered list into min-heap.'''
+
+        # for each node in the first half of the list
+        # percolate down
+        return heapq.heapify(self._data)
